24ac55ce02a65a8eb48633afb78edfa6c1ee6a37
John Ouellet <john@thinktandem.io>
2021-12-21 12:16:48 -0500

    Adjust library and lock

--- recipes/lamp/builder.js
+++ recipes/lamp/builder.js
@@ -1,9 +1,22 @@
 'use strict';
 
-// Modules
-const utils = require('./../../lib/utils');
-
 /*
  * Build LAMP
  */
-module.exports = utils.getLampDefaults();
+module.exports = {
+  name: 'lamp',
+  parent: '_lamp',
+  config: {
+    confSrc: __dirname,
+    database: 'mysql',
+    php: '7.4',
+    via: 'apache',
+    webroot: '.',
+    xdebug: false,
+  },
+  builder: (parent, config) => class LandoLamp extends parent {
+    constructor(id, options = {}) {
+      super(id, _.merge({}, config, options));
+    };
+  },
+};
