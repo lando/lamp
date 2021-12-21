cda097925e16b3afa8385cc2f017c186f2dcb6b7
John Ouellet <john@thinktandem.io>
2021-12-21 12:40:23 -0500

    Add in libraries and tests (#1)
    * Add in libraries and tests
    
    * Adjust library and lock
    
    * Adjust tests

---
+++ recipes/lamp/builder.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/*
+ * Build LAMP
+ */
+module.exports = {
+  name: 'lamp',
+  parent: '_lamp',
+  config: {
+    confSrc: __dirname,
+    database: 'mysql',
+    php: '7.4',
+    via: 'apache',
+    webroot: '.',
+    xdebug: false,
+  },
+  builder: (parent, config) => class LandoLamp extends parent {
+    constructor(id, options = {}) {
+      super(id, _.merge({}, config, options));
+    };
+  },
+};
