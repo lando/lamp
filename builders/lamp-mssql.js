0a345aaf501a2f142de0bd077c8c4a0ce36b01d3
Alec Reynolds <alec+git@lando.dev>
2024-02-20 14:36:24 -0800

    Add in undocumented mssql option for backwards compatibility.

---
+++ builders/lamp-mssql.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const _ = require('lodash');
+const LandoMssql = require('./../node_modules/@lando/mssql/builders/mssql.js');
+
+// Builder
+module.exports = {
+  name: 'lamp-mssql',
+  parent: '_service',
+  builder: (parent, config) => class LampMssql extends LandoMssql.builder(parent, LandoMssql.config) {
+    constructor(id, options = {}) {
+      super(id, _.merge({}, config, options), {services: _.set({}, options.name)});
+    };
+  },
+};
