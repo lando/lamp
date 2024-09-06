b51b40f2d221e13c06081870dd8ae8c16a11d3ca
Mike Pirog <mike@lando.dev>
2024-09-06 09:36:21 -0400

    fix bug causing default recipe proxy url to be clobbered by user specified ones

--- builders/lamp.js
+++ builders/lamp.js
@@ -185,6 +185,18 @@ const getDbTooling = database => {
 };
 
 /*
+ * Helper to get proxy config
+ */
+const getProxy = (options, proxyService = 'appserver') => {
+  // get any intial proxy stuff for proxyService
+  const urls = _.get(options, `_app.config.proxy.${proxyService}`, []);
+  // add
+  urls.push(`${options.app}.${options._app._config.domain}`);
+  // return
+  return {[proxyService]: _.uniq(_.compact(urls))};
+};
+
+/*
  * Helper to get service config
  */
 const getServiceConfig = (options, types = ['php', 'server', 'vhosts']) => {
@@ -227,16 +239,19 @@ module.exports = {
   builder: (parent, config) => class LandoLamp extends parent {
     constructor(id, options = {}) {
       options = _.merge({}, config, options);
-      // Rebase on top of any default config we might already have
-      options.defaultFiles = _.merge({}, getConfigDefaults(_.cloneDeep(options)), options.defaultFiles);
-      options.services = _.merge({}, getServices(options), options.services);
-      options.tooling = _.merge({}, getTooling(options), options.tooling);
+
       // Switch the proxy if needed
       if (!_.has(options, 'proxyService')) {
         if (_.startsWith(options.via, 'nginx')) options.proxyService = 'appserver_nginx';
         else if (_.startsWith(options.via, 'apache')) options.proxyService = 'appserver';
       }
-      options.proxy = _.set(options.proxy, options.proxyService, [`${options.app}.${options._app._config.domain}`]);
+
+      // Rebase on top of any default config we might already have
+      options.defaultFiles = _.merge({}, getConfigDefaults(_.cloneDeep(options)), options.defaultFiles);
+      options.proxy = _.merge({}, getProxy(options, options.proxyService), options.proxy);
+      options.services = _.merge({}, getServices(options), options.services);
+      options.tooling = _.merge({}, getTooling(options), options.tooling);
+
       // Downstream
       super(id, options);
     };
